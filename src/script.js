0dbd164c20a7ff957a2ff981cafd259ce7490460
Wesley Smith <me@wesjd.net>
2020-01-20 15:08:49 -0500

    complete section sorting

--- src/script.js
+++ src/script.js
@@ -1,20 +1,44 @@
-const elements = {
-    selectedName: "all"
-}
+const sorts = ["all", "study", "podcast", "video"]
+
+let elements
 
 document.addEventListener("DOMContentLoaded", () => {
-    elements.categories = document.getElementsByClassName("category")
-    elements.psychUnits = document.getElementsByClassName("psych-unit")
+    elements = {
+        categories: document.getElementsByClassName("category"),
+        psychunits: document.getElementsByClassName("psych-unit"),
+        noneFound: document.querySelector("[name='none-found']"),
+        number: document.getElementById("results-number"),
+        sortPsychUnit: document.getElementById("sort-psych-unit"),
+        selected: {
+            name: "all",
+            element: document.querySelector("[name='all']"),
+            buttons: [document.getElementById("sort-all")]
+        }
+    }
 
-    elements.current = document.querySelector("[name='all']")
-    bind("sort-all", () => select("all", "categories"))
-    bind("sort-studies", () => select("study", "categories"))
-    bind("sort-podcasts", () => select("podcast", "categories"))
-    bind("sort-videos", () => select("video", "categories"))
+
+    updateResultsNumber()
+
+    sorts.forEach(name => {
+        const element = document.getElementById(`sort-${name}`)
+        element.addEventListener("click", () => {
+            select(name, "categories", element)
+        })
+    })
+
+    document.querySelectorAll(".tag-selector").forEach(element => {
+        element.addEventListener("click", () => {
+            select(element.innerHTML, "psychunits", element)
+        })
+    })
 })
 
-function bind(elementId, func) {
-    document.getElementById(elementId).addEventListener("click", func)
+function updateResultsNumber() {
+    let amount = 0
+    if (elements.selected.element != elements.noneFound) {
+        amount = elements.selected.element.children.length
+    }
+    elements.number.innerHTML = amount
 }
 
 function findElementByName(list, name) {
@@ -25,32 +49,34 @@ function findElementByName(list, name) {
     }
 }
 
-function select(name, type) {
-    if (name == elements.selectedName) {
+function select(name, type, button) {
+    if (name == elements.selected.name) {
         return
     }
 
-    const list = elements[type.toLowerCase()]
+    const list = elements[type = type.toLowerCase()]
     if (!list) {
         console.log(new Error("Could not find type of element"))
         return
     }
 
-    if (elements.current) {
-        toggleClass(elements.current, "is-hidden")
-    }
+    const element = findElementByName(list, name) || elements.noneFound
 
-    elements.selectedName = name
-
-    const element = findElementByName(list, name)
-    if (!element) {
-        //TODO - make nothing found element show
-        console.log(new Error("Could not find element by name"))
-        return
-    }
-
+    toggleClass(elements.selected.element, "is-hidden")
     toggleClass(element, "is-hidden")
-    elements.current = element
+
+    elements.selected.buttons.forEach(button => toggleClass(button, "is-active"))
+    const buttons = [button]
+    if (type == "psychunits") {
+        buttons.push(elements.sortPsychUnit)
+    }
+    buttons.forEach(button => toggleClass(button, "is-active"))
+
+    elements.selected.element = element
+    elements.selected.name = name
+    elements.selected.buttons = buttons
+
+    updateResultsNumber()
 }
 
 function toggleClass(element, className) {
