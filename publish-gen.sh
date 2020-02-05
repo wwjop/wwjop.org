248804e0dc06e5b5d487ef630fe0e063f9be6358
Wesley Smith <me@wesleysmith.dev>
2020-02-05 18:53:01 +0000

    begin creating files in publish-gen

--- publish-gen.sh
+++ publish-gen.sh
@@ -16,12 +16,15 @@ echo
 read -p "At what school was the article written? " school_name
 
 echo
+read -p "What is the post date for the article? (Year-Month-Day; ex: 2020-01-25) " post_date
+
+echo
 read_num_authors
 author_names=()
-for ((i = 1 ; i <= $authors_count ; i++)); do
+for ((i = 1; i <= $authors_count; i++)); do
     echo "------ Author $i ------"
     read -p "Full Name (First Last): " author_name
-    author_names+=($author_name)
+    author_names+=( "$author_name" )
 done
 
 echo
@@ -31,9 +34,71 @@ for (( i=0; i<${#AVAILABLE_PSYCH_UNITS[@]}; i++ )); do
     read -p "${AVAILABLE_PSYCH_UNITS[$i]} (Y/N): " response 
     case "$response" in
         [yY][eE][sS]|[yY]) 
-            selected_categories+=(${AVAILABLE_PSYCH_UNITS[$i]})
+            selected_categories+=( "${AVAILABLE_PSYCH_UNITS[$i]}" )
             ;;
         *)
             ;;
     esac
 done
+
+echo
+echo "---- Article Information ----"
+echo "Name: $article_name"
+echo "School: $school_name"
+echo "Post date (Year-Month-Day; ex: 2020-01-25): $post_date"
+
+if (( ${#author_names[@]} > 0 )); then
+    authors="${author_names[0]}"
+    for ((i = 1; i <= ${#author_names[@]}; i++)); do
+        authors="$authors, ${author_names[$i]}"
+    done
+    echo "$authors_count authors: $authors"
+else
+    echo "0 authors"
+fi
+
+if (( ${#selected_categories[@]} > 0 )); then
+    categories="${selected_categories[0]}"
+    for ((i = 1; i <= ${#selected_categories[@]}; i++)); do
+        categories="$categories, ${selected_categories[$i]}"
+    done
+    echo "${#selected_categories[@]} categories: $categories"
+else 
+    echo "0 categories"
+fi
+
+echo
+read -p "Is this information all correct? [Y/N]: " confirm
+case "$confirm" in
+    [yY][eE][sS]|[yY]) 
+        ;;
+    *)
+        echo "Exiting..."
+        exit
+        ;;
+esac
+
+echo
+echo "Generating files..."
+
+_post_file_name_article_name="${article_name//\ /\-}"
+_posts_file_name="$post_date-${_post_file_name_article_name,,}.html"
+_post_file="src/_posts/$_posts_file_name"
+echo
+echo "Post file location: $_post_file"
+touch "$_post_file"
+
+echo "---" >> $post_file
+echo "title: '$article_name'" >> $post_file
+if (( ${#author_names[@]} > 0 )); then
+    authors="[ '${author_names[0]}''"
+    for ((i = 1; i <= ${#author_names[@]}; i++)); do
+        authors="$authors, '${author_names[$i]}''"
+    done
+    authors="$authors]"
+    echo "authors: $authors"
+fi
+echo "school: $school_name" >> $post_file
+echo "category: 'study'" >> $post_file
+echo "tags: "
+echo "---" >> $post_file
