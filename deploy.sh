e745e4a2cec37c6f48b130a8a9ad35c5edbdab0f
whitmanpsych <whitmanpsych@gmail.com>
2020-03-06 19:02:14 +0000

    Updated staff members spring 2020

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-prompt -p "Type a description of your changes for this deployment: " message
+read -p "Type a description of your changes for this deployment: " message
 
 git add .
 git commit -m "$message"
